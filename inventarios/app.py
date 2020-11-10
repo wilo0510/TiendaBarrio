3bf3f1999db763bac0894728b1b90090229518f6
Wilmer <wrpachonl@gmail.com>
2020-11-09 20:49:30 -0500

    Initial commit

---
+++ inventarios/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/inventarios")
+def listProducts():
+    return str("Productos")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
