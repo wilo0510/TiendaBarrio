394dbe5c76c32ada95329696a26416a37e993fcf
shootface <wava0324@gmail.com>
2020-11-12 20:49:40 -0500

    adding user list

--- inventarios/app.py
+++ inventarios/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/inventarios")
 def listProducts():
-    return str("Productos")
+    return str("PRRRR")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
--- usuarios/app.py
+++ usuarios/app.py
@@ -1,10 +1,40 @@
 from flask import Flask
+import pymysql
 
 app = Flask(__name__)
 
+class DataBase:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host = 'localhost',
+            user = 'root',
+            password = 'root',
+            db = 'db_usuarios'
+        )
+        self.cursor = self.connection.cursor()
+    
+    def select_user(self, id):
+        sql = 'SELECT id,name,email from usuario where id = {}'.format(id)
+        try:
+            self.cursor.execute(sql)
+            user = self.cursor.fetchone()
+        except Exception as e:
+            raise
+    
+    def select_all_user(self):
+        sql = 'SELECT id,name,email from usuario'
+        try:
+            self.cursor.execute(sql)
+            users = self.cursor.fetchall()
+            return users
+        except expression as identifier:
+            pass
+
 @app.route("/usuarios")
 def listUsers():
-    return str("usuarios")
+    return str("HOLA")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
+
+
