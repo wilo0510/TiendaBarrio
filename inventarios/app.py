382aec650cee8b0622a2aaefe40e779cd541e344
shootface <wava0324@gmail.com>
2020-11-29 13:10:34 -0500

    inventary service done

--- inventarios/app.py
+++ inventarios/app.py
@@ -1,10 +1,45 @@
 from flask import Flask
+import pymysql
 
 app = Flask(__name__)
 
+class DataBase:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host = 'mysql_database',
+            user = 'root',
+            password = 'root',
+            db = 'db_INVENTARIOS'
+        )
+        self.cursor = self.connection.cursor()
+    
+    def select_product(self, id):
+        sql = 'SELECT * from inventario where idProducto = {}'.format(id)
+        try:
+            self.cursor.execute(sql)
+            user = self.cursor.fetchone()
+            return user
+        except Exception as e:
+            raise
+    
+    def select_all_product(self):
+        sql = 'SELECT idProducto,ubicacion from inventario'
+        try:
+            self.cursor.execute(sql)
+            prods = self.cursor.fetchall()
+            return prods
+        except expression as identifier:
+            pass
+
 @app.route("/inventarios")
 def listProducts():
-    return str("PRRRR")
+    db = DataBase()
+    return str(db.select_all_product())
+
+@app.route("/inventarios/<int:pk>")
+def getProduct(pk):
+    db = DataBase()
+    return str(db.select_product(pk))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
