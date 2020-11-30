039579a195761abe4c58412bb35e796694866f18
shootface <wava0324@gmail.com>
2020-11-29 21:48:57 -0500

    you can make request to "pedidos"

--- pedidos/app.py
+++ pedidos/app.py
@@ -14,15 +14,27 @@ class DataBase:
         )
         self.cursor = self.connection.cursor()
 
-    def regisPedido(self, idPro):
-        prod = requests.get('http://localhost:8080/inventarios')
-        return prod
+    def regisPedido(self,idPro,idUse, can):
+        prod = requests.get('http://inventarios:5000/inventarios/'+str(idPro))
+        user = requests.get('http://usuarios:5000/usuarios/'+str(idUse))
+        prodid = str(idPro).split(',')
+        userid = str(idUse).split(',')
+        sql = 'insert into db_pedidos.pedidos (idProducto,idUsuario,cantidad) values ('+ prodid[0] + ',' + userid[0] +','+ str(can)+')'
+        print(sql)
+        try:
+            self.cursor.execute(sql) 
+            self.connection.commit()
+            #prods = self.cursor.fetchall()
+            #return prods 
+        except Exception as e:
+            print(e) 
+            raise
 
 
-@app.route("/pedidos")
-def pedido():
+@app.route("/pedidos/<int:idPro>/<int:idUse>/<int:cantidad>", methods = ['POST'])
+def pedido(idPro,idUse, cantidad):
     db = DataBase()
-    return str(db.regisPedido(2568))
+    return str(db.regisPedido(idPro,idUse,cantidad))
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+if __name__ == "__main__": 
+    app.run(host='0.0.0.0',debug=True) 
